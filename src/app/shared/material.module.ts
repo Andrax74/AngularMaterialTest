7c61a39102954ae3bfe57b671ba5cf6d258f4ca0
Nicola La Rocca <nicola.larocca@xantrix.it>
2020-12-24 17:46:00 +0100

    Primo Commit

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,14 @@
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule,
+  ]
+})
+export class MaterialModule { }
