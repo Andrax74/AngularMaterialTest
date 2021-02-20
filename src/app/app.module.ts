1f02b7aec3be94bc6e22f6a0478ec064b8c6204e
Nicola La Rocca <nicola.larocca@xantrix.it>
2021-02-20 16:48:43 +0100

    modifiche varie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from './shared/material.module';
 import { NgModule } from '@angular/core';
 
 const routes: Routes = [
   {path:'demo', loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule)},
-  {path:'gestfid', loadChildren: () => import('./gestfid/gestfid.module').then(m => m.GestfidModule)},
-  {path:'**', redirectTo: 'gestfid'}
+  {path:'**', redirectTo: 'demo'}
 ];
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule,
     RouterModule.forRoot(routes),
   ],
   providers: [],
